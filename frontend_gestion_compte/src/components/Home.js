ec37350101422b1d7c4c4d63fc241be1a23ff7f6
Geffrey SAID <geffrey.dev@gmail.com>
2021-03-21 15:43:45 +0400

    Modify Home design

--- frontend_gestion_compte/src/components/Home.js
+++ frontend_gestion_compte/src/components/Home.js
@@ -3,31 +3,18 @@ import QuickViewCustomer from './QuickViewCustomer';
 import '../styles/Home.css';
 import { useHistory } from 'react-router-dom';
 
-function Home({customers}) {
+function Home({ customers }) {
 
     let history = useHistory();
-
-    
-
-    // const [customers, setCustomers] = useState([]);
-
-
-    // useEffect(() => {
-    //     fetch(process.env.REACT_APP_API_URL + '/customers').then((response) => {
-    //         return response.json();
-    //     }).then((response) => {
-    //         setCustomers(response);
-    //     })
-    // }, [])
 
     const ShowCustomer = (customer) => {
         history.push('/customers/' + customer.name);
     }
 
     return (
-        <div className="container">
+        <div className="container-fluid">
             {customers !== undefined &&
-                <div className="row">
+                <div className="col-12 col-md-6">
                     {customers.map((customer, i) => {
                         return (<QuickViewCustomer customer={customer} key={i} onClick={() => ShowCustomer(customer)} className='quickView' />)
                     })}
--- frontend_gestion_compte/src/components/QuickViewCustomer.js
+++ frontend_gestion_compte/src/components/QuickViewCustomer.js
@@ -9,7 +9,7 @@ const QuickViewCustomer = (props) => {
 
     const { onClick } = props;
     return (
-        <div className="quickView col-md-3" onClick={onClick}>
+        <div className="quickView" onClick={onClick}>
             <h4>{props.customer.name}</h4>
             <div className="row text-center">
                 <div className="col-12">{calculTotals(props.customer)},00 €</div>
