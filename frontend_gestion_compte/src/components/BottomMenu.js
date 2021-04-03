85f39fc4f80a084f8a1ddfe49fb7982f69f43637
Geffrey SAID <geffrey.dev@gmail.com>
2021-04-04 01:15:41 +0400

    Modified Navbar design

--- frontend_gestion_compte/src/components/BottomMenu.js
+++ frontend_gestion_compte/src/components/BottomMenu.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import '../App.css'
+import '../styles/BottomMenu.css';
 import { MdDashboard, MdSupervisorAccount, MdList } from "react-icons/md";
-import { VscDeviceMobile } from "react-icons/vsc";
 import { NavLink } from 'react-router-dom';
 
 function BottomMenu() {
     return (
-        <nav className="nav d-flex justify-content-around">
-            <NavLink to='/mb/home' activeClassName='current' className='nav_link text-center'> <VscDeviceMobile size={25} /> <br /> <span className="nav_text">Mobile</span> </NavLink>
-            <NavLink to='/customers' exact activeClassName='current' className="nav_link text-center"><MdSupervisorAccount size={25} /> <br /> <span className="nav_text"> Clients</span></NavLink>
-            <NavLink to='/transactions' exact activeClassName='current' className="nav_link text-center"><MdList size={25} /> <br /> <span className="nav_text"> Transaction</span></NavLink>
+        <nav className="nav">
+            <NavLink to='/mb/home' activeClassName='current' className='nav_link text-center'> <MdDashboard size={20} /> <br /> <span className="nav_text">Accueil</span> </NavLink>
+            <NavLink to='/customers' exact activeClassName='current' className="nav_link text-center"><MdSupervisorAccount size={20} /> <br /> <span className="nav_text"> Clients</span></NavLink>
+            <NavLink to='/transactions' exact activeClassName='current' className="nav_link text-center"><MdList size={20} /> <br /> <span className="nav_text"> Transaction</span></NavLink>
         </nav>
     )
 }
