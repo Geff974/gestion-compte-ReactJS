dfe997fd091ef541f54e217f97e803fe8964bcb0
Geff974 <geffrey.dev@gmail.com>
2021-06-09 19:58:32 +0400

    Pofine graphic

--- frontend_gestion_compte/src/pages/Account.jsx
+++ frontend_gestion_compte/src/pages/Account.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
 import { Line } from 'react-chartjs-2';
+import '../styles/Account.css';
 
 const Account = () => {
 
     const transactions = useSelector(state => state.transactions.transactions)
     const month = ['JANVIER', 'FEVRIER', 'MARS', 'AVRIL', 'MAI', 'JUIN', 'JUILLET', 'AOUT', 'SEPTEMBRE', 'OCTOBRE', 'NOVEMBRE', 'DECEMBRE'];
     const positifAmounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    const negatifAmounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let negatifAmounts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const currentMonth = new Date().getMonth();
+
+    month.splice(currentMonth + 1, month.length - 1);
 
     transactions.forEach(trans => {
         const monthTrans = new Date(trans.date).getMonth();
         const year = new Date(trans.date).getFullYear();
-        console.log('year ' + year);
-        console.log('new : ' + new Date().getFullYear())
         if (new Date(trans.date).getFullYear() === new Date().getFullYear()) {
             if (trans.amount > 0) {
                 positifAmounts[monthTrans] = positifAmounts[monthTrans] + trans.amount;
@@ -23,32 +26,52 @@ const Account = () => {
         }
     });
 
+    negatifAmounts.forEach((el, i) => {
+        negatifAmounts[i] = -el;
+    })
+
+
+    const data = {
+        labels: month,
+        datasets: [
+            {
+                label: 'Facture',
+                backgroundColor: 'green',
+                data: positifAmounts
+            },
+            {
+                label: 'Paiement',
+                backgroundColor: 'blue',
+                data: negatifAmounts
+            }
+        ]
+    }
+
     const test = () => {
         console.log(positifAmounts);
         console.log(negatifAmounts);
     }
 
     return (
-        <div>
-            <h1 onClick={test}>Statistic</h1>
+        <div className="account-component">
+            <h1>Statistic</h1>
 
             <Line
-                data={{
-                    labels: month,
-                    // label: ['Red', 'Blue', 'Yellow', 'Purple', 'Green', 'Orange'],
-                    datasets: [
-                        {
-                            label: 'Facture',
-                            data: positifAmounts,
-                            backgroundcolor: 'red'
-                        }
-                    ]
+                data={data}
+                // height={200}
+                options={{
+                    responsive: true,
+                    // maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [
+                            {
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }
+                        ]
+                    }
                 }}
-                height={200}
-            // width={200}
-            // options={{
-            //     maintainAspectRatio: false
-            // }}
             />
         </div>
     );
